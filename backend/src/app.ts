c159aeff4625ecef142287f7d9346915c62178b1
kreeti1210 <kreetisharma1210@gmail.com>
2026-05-27 18:09:47 +0530

    Small fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://doodhwala-tracker.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
